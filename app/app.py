24485319df780a1be4430bd9e1689099fcbfb201
Divyaa <divyaadwi@gmail.com>
2026-05-21 23:51:04 +0530

    modify app

--- app/app.py
+++ app/app.py
@@ -1,24 +1,56 @@
 from flask import Flask
 import random
+import time
 
 app = Flask(__name__)
 
-logs = [
-    "INFO: Request received",
-    "WARNING: High response time",
-    "ERROR: Database timeout",
-    "INFO: Health check passed"
-]
-
 @app.route("/")
 def home():
-    log = random.choice(logs)
-    print(log, flush=True)
+    print("INFO: Home endpoint accessed", flush=True)
     return "K8s AI Log Analyzer Running"
+
+@app.route("/login")
+def login():
+    users = ["admin", "guest", "test-user"]
+
+    user = random.choice(users)
+
+    print(f"INFO: User login attempt: {user}", flush=True)
+
+    return "Login processed"
+
+@app.route("/payment")
+def payment():
+    outcomes = [
+        "INFO: Payment processed successfully",
+        "WARNING: Payment gateway slow response",
+        "ERROR: Payment service timeout"
+    ]
+
+    log = random.choice(outcomes)
+
+    print(log, flush=True)
+
+    return "Payment request completed"
+
+@app.route("/database")
+def database():
+    print("WARNING: Database query latency high", flush=True)
+
+    time.sleep(2)
+
+    return "Database query executed"
+
+@app.route("/health")
+def health():
+    print("INFO: Health check passed", flush=True)
+
+    return "Healthy"
 
 @app.route("/error")
 def error():
-    print("ERROR: Simulated application failure", flush=True)
+    print("ERROR: Application crash simulated", flush=True)
+
     return "Error generated"
 
 if __name__ == "__main__":
